7c7558a378a926512ef8d97b8c87680967849345
DandinPower <t108590045@ntut.org.tw>
2022-03-24 21:51:28 +0800

    fix:chap2 birthday import fix

--- hw1/project/chap2/part2/birthday.c
+++ hw1/project/chap2/part2/birthday.c
@@ -1,6 +1,9 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/list.h>
+#include <linux/types.h> 
+#include <linux/slab.h>
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("BirthdayModule");
