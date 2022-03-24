470d6d0e40850ad7904c8af8a8c22afe109fe79b
DandinPower <t108590045@ntut.org.tw>
2022-03-25 00:01:20 +0800

    add: chap3 project1 init

---
+++ hw1/project/chap3/project1/shell.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+
+#define MAX_LEN 80
+
+int main(void){
+    while(1){
+        char *args[MAX_LEN/2 + 1];
+        char input[MAX_LEN];
+        printf("osh>");
+        gets(input);
+        char *p = strtok(input, " ");
+        int i = 0;
+        args[i] = p;
+        while(p != NULL){
+            i++;
+            p = strtok(NULL, " ");
+            args[i] = p;
+        }
+        args[i+1] = NULL;
+        //args已經完成
+        pid_t pid;
+        pid = fork();
+        if (pid == 0) {
+            for(int j=0;j<=i;j++){
+                printf("%s\n",args[j]);
+            }
+        }
+        else {
+            wait(NULL);
+            printf("Child Complete\n");
+        }
+
+    }
+    return 0;
+}
