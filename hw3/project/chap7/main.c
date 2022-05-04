d517c0dfcc3973f0d8f45a053ef2fa45877d64b2
DandinPower <t108590045@ntut.org.tw>
2022-05-04 17:28:49 +0800

    add: random generate request,release

--- hw3/project/chap7/main.c
+++ hw3/project/chap7/main.c
@@ -10,6 +10,15 @@
 #define RESOURCES_NUMS 4
 #define DISABLE_VALUE 9999
 
+void InitializeAvailable(char *argv[]);
+void InitializeMaximum();
+int WhichIsAvailable(int availableList[RESOURCES_NUMS], int needList[CUSTOMERS_NUMS][RESOURCES_NUMS], int pass[CUSTOMERS_NUMS]);
+int CheckSafe(int availableList[RESOURCES_NUMS],int allocationList[CUSTOMERS_NUMS][RESOURCES_NUMS],int needList[CUSTOMERS_NUMS][RESOURCES_NUMS]);
+void *CustomerRunner(void *param);
+int RequestResources(int customerIndex, int requestList[RESOURCES_NUMS]);
+void ReleaseResources(int customerIndex, int releaseList[RESOURCES_NUMS]);
+void ShowData();
+
 //用來保護不同customer不會產生race condition
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 //pthread_mutex_lock(&mutex);
@@ -89,12 +98,80 @@ int CheckSafe(int availableList[RESOURCES_NUMS],int allocationList[CUSTOMERS_NUM
     return 0;
 }
 
+//customer request
+int RequestResources(int customerIndex, int requestList[RESOURCES_NUMS]){
+    int error = 0;
+    for (int i=0; i<RESOURCES_NUMS; i++) {
+        available[i] -= requestList[i];
+        allocation[customerIndex][i] += requestList[i];
+        need[customerIndex][i] -= requestList[i];
+        if (need[customerIndex][i] < 0) error = 1;
+    }
+    int state = CheckSafe(available,allocation,need);
+    //需要回復狀態
+    if (error || state == -1){
+        for (int i=0; i<RESOURCES_NUMS; i++) {
+            available[i] += requestList[i];
+            allocation[customerIndex][i] -= requestList[i];
+            need[customerIndex][i] += requestList[i];
+        }
+        if (error) printf("Invalid Request because request more than need\n");
+        if (state == -1) printf("Request is Reject because can't find sequence,so it's in unsafe state\n");
+    }
+    return state;
+}
+
+//customer release
+void ReleaseResources(int customerIndex, int releaseList[RESOURCES_NUMS]){
+    int error = 0;
+    for (int i=0; i<RESOURCES_NUMS; i++) {
+        available[i] += releaseList[i];
+        allocation[customerIndex][i] -= releaseList[i];
+        need[customerIndex][i] += releaseList[i];
+        if (allocation[customerIndex][i] < 0) error = 1;
+    }
+    //需要回復狀態
+    if (error){
+        for (int i=0; i<RESOURCES_NUMS; i++) {
+            available[i] -= releaseList[i];
+            allocation[customerIndex][i] += releaseList[i];
+            need[customerIndex][i] -= releaseList[i];
+        }
+        printf("Invalid Release because release more than allocation\n");
+    }
+}
+
+//根據該customer的need隨機產生request
+void RandomRequest(int index, int request[RESOURCES_NUMS]){
+    for (int j=0; j<RESOURCES_NUMS; j++) {
+        request[j] = rand()%(need[index][j]+1);
+    }
+}
+
+//根據該customer的allocation隨機產生release
+void RandomRelease()(int index, int release[RESOURCES_NUMS]){
+    for (int j=0; j<RESOURCES_NUMS; j++) {
+        release[j] = rand()%(allocation[index][j]+1);
+    }
+}
+
 //customer thread
 void *CustomerRunner(void *param){
     int* index = (int*)param;
+    int requestRandomList[RESOURCES_NUMS];
+    int releaseRandomList[RESOURCES_NUMS];
+    RandomRequest(*index, requestRandomList);
+    RandomRelease(*index, releaseRandomList);
+    printf("[%d]customer is request: ");
+    for (int j=0; j<RESOURCES_NUMS; j++) printf("%d ",requestRandomList[j]);
+    printf("\n");
+    printf("[%d]customer is release: ");
+    for (int j=0; j<RESOURCES_NUMS; j++) printf("%d ",releaseRandomList[j]);
+    printf("\n");
     pthread_exit(0);
 }
 
+//印出狀態
 void ShowData(){
     printf("Available:\n");
     for(int j=0; j<RESOURCES_NUMS; j++){
