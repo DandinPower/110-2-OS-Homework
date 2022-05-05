1e49e8098450523cd5c10527cf02b00468a72d43
DandinPower <t108590045@ntut.org.tw>
2022-05-05 18:58:36 +0800

    add: OPA finish

--- hw3/problem/9.26/main.c
+++ hw3/problem/9.26/main.c
@@ -3,6 +3,7 @@
 #include<string.h>
 #include<time.h>
 
+#define MAX_LIMIT 999
 #define PAGE_NUMS 20
 
 int pageSequence[PAGE_NUMS] = {7, 2, 3, 1, 2, 5, 3, 4, 6, 7, 7, 1, 0, 5, 4, 6, 2, 3, 0, 1};
@@ -15,6 +16,7 @@ int GetRandomNumber(){
 
 //打亂pageSequence
 void GetRandomSequence(){
+    printf("Random Generate Sequence: \n");
     for (int i=0; i<PAGE_NUMS; i++){
         pageSequence[i] = GetRandomNumber();
     }
@@ -85,10 +87,88 @@ void LRU(){
     printf("LRU page faults: %d\n",faults);
 }
 
+//找出該page在一定範圍內出現的位置
+int FindPageIndex(int pageIndex, int start){
+    for (int i=start; i<PAGE_NUMS; i++) if(pageSequence[i] == pageIndex) return i;
+    return -1;
+}
+
+//根據state找出該換的那一個
+int FindExchange(int *state, int current){
+    int max = -1;
+    int maxIndex = -1;
+    int tempIndex;
+    int nonFindNums = 0;
+    for (int i=0; i<frameNums; i++){
+        tempIndex = FindPageIndex(state[i], current+1);
+        //printf("tempIndex: %d\n",tempIndex);
+        if (tempIndex > maxIndex || tempIndex == -1) {
+            maxIndex = tempIndex;
+            if (tempIndex == -1) {
+                maxIndex = MAX_LIMIT + nonFindNums;
+                nonFindNums++;
+            }
+            max = state[i];
+        }
+    }
+    return max;
+}
+
+//找出該值在state的位置
+int FindPageIndexInStateIndex(int *state, int pageIndex){
+    for (int i=0; i< frameNums; i++) if(state[i]==pageIndex) return i;
+}
+
+//將page加進optimal的state
+void AddOPAState(int *state, int pageIndex, int current){
+    int initstate = 0;
+    for (int i=0; i<frameNums; i++) {
+        if(state[i] == -1){
+            state[i] = pageIndex;
+            initstate = 1;
+            break;
+        }
+    }
+    if (initstate == 0){
+        int exchange = FindExchange(state, current);
+        //printf("exchange: %d ,",exchange);
+        int exchangeStateIndex = FindPageIndexInStateIndex(state, exchange);
+        UpdateLRUState(state, pageIndex, exchangeStateIndex);
+    }
+}
+
+//實現optimal algorithm
+void OPA(){
+    int faults = 0;
+    int *state;
+    state = (int *)malloc(sizeof(int) * frameNums);
+    for (int i=0; i<frameNums; i++) state[i] = -1;
+    for (int i=0; i<PAGE_NUMS; i++){
+        int index = CheckIsInFrames(state, pageSequence[i]);
+        if(index == -1){
+            faults++;
+            AddOPAState(state, pageSequence[i], i);
+        }
+        //ShowState(state);
+    }
+    printf("OPA page faults: %d\n",faults);
+}
+
+//實現所有algorithm
+void All(){
+    ShowSequence();
+    printf("frames nums is: %d\n",frameNums);
+    FIFO();
+    LRU();
+    OPA();
+    printf("\n\n");
+}
+
 int main(int argc,char* argv[]){
     srand(time(NULL));
     frameNums = atoi(argv[1]);
-    FIFO();
-    LRU();
+    All();
+    GetRandomSequence();
+    All();
     return 0;
 }
