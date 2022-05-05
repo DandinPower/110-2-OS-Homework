c16aa9a1dee2a7a2adff261def7045d966e24797
DandinPower <t108590045@ntut.org.tw>
2022-05-05 18:07:06 +0800

    add: 9.26 FIFO finish

---
+++ hw3/problem/9.26/main.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+
+#define PAGE_NUMS 20
+
+int pageSequence[PAGE_NUMS] = {7, 2, 3, 1, 2, 5, 3, 4, 6, 7, 7, 1, 0, 5, 4, 6, 2, 3, 0, 1};
+int frameNums = 3;
+
+//取得一個隨機0~9的數
+int GetRandomNumber(){
+    return (rand()%10);
+}
+
+//打亂pageSequence
+void GetRandomSequence(){
+    for (int i=0; i<PAGE_NUMS; i++){
+        pageSequence[i] = GetRandomNumber();
+    }
+}
+
+//展示目前的list
+void ShowSequence(){
+    for (int i=0; i<PAGE_NUMS; i++) printf("%d ",pageSequence[i]);
+    printf("\n");
+}
+
+//展示目前的state
+void ShowState(int *state){
+    for (int i=0; i<frameNums; i++) printf("%d ",state[i]);
+    printf("\n");
+}
+
+//檢查該page有無在frames裡
+int CheckIsInFrames(int *state, int pageIndex){
+    int answer = 0;
+    for (int i=0; i<frameNums; i++) if(state[i]==pageIndex) answer = 1;
+    return answer;
+}
+
+//將pageIndex加進state(FIFO)
+void AddFIFOState(int *state, int pageIndex){
+    if(state[0]!=-1) for (int i=frameNums-1; i>0; i--) state[i] = state[i-1];
+    state[0] = pageIndex;
+}
+
+//實現fifo algorithm
+void FIFO(){
+    int faults = 0;
+    int *state;
+    state = (int *)malloc(sizeof(int) * frameNums);
+    for (int i=0; i<frameNums; i++) state[i] = -1;
+    for (int i=0; i<PAGE_NUMS; i++){
+        if(CheckIsInFrames(state, pageSequence[i])==0){
+            faults++;
+            AddFIFOState(state, pageSequence[i]);
+        }
+    }
+    printf("FIFO page faults: %d\n",faults);
+}
+
+int main(int argc,char* argv[]){
+    srand(time(NULL));
+    frameNums = atoi(argv[1]);
+    FIFO();
+    return 0;
+}
