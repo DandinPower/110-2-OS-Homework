89fce97a60b4a5b44038222da89e7ef1b4db941f
DandinPower <t108590045@ntut.org.tw>
2022-05-30 16:27:33 +0800

    hw4: finial

--- hw4/problem/12.16/main.c
+++ hw4/problem/12.16/main.c
@@ -156,6 +156,34 @@ int SCAN(){
     return total;
 }
 
+int SCAN2(){
+    int total = 0;
+    int *sortList = CopyList(waitingList);
+    SortArray(sortList);
+    int biggerIndex = FindBiggerElement(sortList);
+    int smallLength = biggerIndex;
+    int bigLength = LIST_LENGTH - biggerIndex;
+    int* smallList = GetSmallerSlice(sortList,smallLength);
+    smallList = ReverseArray(smallList,smallLength);
+    smallList = AppendArray(smallList,smallLength,START);
+    smallLength++;
+    int* bigList = GetBiggerSlice(sortList,biggerIndex);
+    printf("%d ",head);
+    int current = head;
+    for (int i=0; i < smallLength; i++){
+        printf("%d ",smallList[i]);
+        total = total + abs(smallList[i] - current);
+        current = smallList[i];
+    }
+    for (int i=0; i < bigLength; i++){
+        printf("%d ",bigList[i]);
+        total = total + abs(bigList[i] - current);
+        current = bigList[i];
+    }
+    printf("\n");
+    return total;
+}
+
 int CSCAN(){
     int total = 0;
     int *sortList = CopyList(waitingList);
@@ -176,7 +204,6 @@ int CSCAN(){
         total = total + abs(bigList[i] - current);
         current = bigList[i];
     }
-    printf("\n");
     for (int i=0; i < smallLength; i++){
         printf("%d ",smallList[i]);
         total = total + abs(smallList[i] - current);
@@ -188,8 +215,9 @@ int CSCAN(){
 
 int main(int argc, char *argv[]){
     head = atoi(argv[1]);
-    printf("FCFS: %d\n",FCFS());
-    printf("SSTF: %d\n",SSTF());
-    printf("SCAN: %d\n",SCAN());
-    printf("CSCAN: %d\n",CSCAN());
+    printf("FCFS: %d\n\n",FCFS());
+    printf("SSTF: %d\n\n",SSTF());
+    printf("SCAN(up first): %d\n\n",SCAN());
+    printf("SCAN(down first): %d\n\n",SCAN2());
+    printf("CSCAN: %d\n\n",CSCAN());
 }
